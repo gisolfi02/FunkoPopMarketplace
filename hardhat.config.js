b033065a6e7ddbe062711e2d807598f9652b9035
Andrea <114088374+gisolfi02@users.noreply.github.com>
2025-12-11 19:16:27 +0100

    Updates favicon

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,13 +6,13 @@ module.exports = {
     version: "0.8.30",
     settings: {
       optimizer: { enabled: true, runs: 200 },
-      viaIR: true
-    }
+      viaIR: true,
+    },
   },
   networks: {
     ganache: {
-      url: "http://127.0.0.1:7545",   // Porta predefinita Ganache GUI
-      accounts: [process.env.GANACHE_PRIVATE_KEY]
-    }
-  }
+      url: "http://127.0.0.1:7545", // Porta predefinita Ganache GUI
+      accounts: [process.env.GANACHE_PRIVATE_KEY],
+    },
+  },
 };
